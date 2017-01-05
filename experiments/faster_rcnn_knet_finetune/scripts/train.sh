35e835d819e56346ffe9d3e304033eeb10d1506f
Sergey Prokudin <sergey.prokudin@gmail.com>
2017-01-05 18:29:51 +0100

    submission script modified

---
+++ experiments/faster_rcnn_knet_finetune/scripts/train.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+
+EXPERIMENT_DIR="${SCRIPT_DIR}/.."
+
+PROJECT_DIR="${SCRIPT_DIR}/../../.."
+
+source "${PROJECT_DIR}/scripts/dbash.sh" || exit 1
+
+dbash::cluster_cuda
+dbash::mac_cuda
+
+cd ${PROJECT_DIR}
+
+set -x
+
+${PYENV_BIN} experiments/faster_rcnn_knet_finetune/model/train.py  \
+            --data_dir="${EXPERIMENT_DIR}/data/pascal_voc_2007/" \
+            --log_dir="${EXPERIMENT_DIR}/logs/pascal_voc_2007/" \
+            --pos_weight=100 \
+            --softmax_loss=False \
+            --softmax_kernel=True \
+            --n_kernels=1\
+            --start_from_scratch=True
